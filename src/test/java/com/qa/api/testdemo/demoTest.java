2fac97f8362ef3646aeecac3c432d431a9a23e5b
tanmay <tanmay.dandapat@ril.com>
2021-01-19 23:09:33 +0530

    first commit for demoRepo project

---
+++ src/test/java/com/qa/api/testdemo/demoTest.java
@@ -0,0 +1,12 @@
+package com.qa.api.testdemo;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	
+	@Test
+    public void demotestcase1 () {
+    	System.out.println("demo test java");
+    }
+
+}
